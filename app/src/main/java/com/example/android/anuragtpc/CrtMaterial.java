f15c388430bc47de3052431189cded4f7a269e52
Thanmai Gajam <thanmai.gajam@gmail.com>
2016-08-25 21:21:16 +0530

    Home Screen with About us 1.

---
+++ app/src/main/java/com/example/android/anuragtpc/CrtMaterial.java
@@ -0,0 +1,16 @@
+package com.example.android.anuragtpc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by thanmaigajam on 25-08-2016.
+ */
+public class CrtMaterial extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.crt_material);
+
+    }
+}
---
+++ app/src/main/java/com/example/android/anuragtpc/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.android.anuragtpc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton AboutUs;
+    ImageButton Contact;
+    ImageButton CrtMaterial;
+    ImageButton NewsFeed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        AboutUs =(ImageButton)findViewById(R.id.imageButton2);
+        AboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AboutUs.class);
+                startActivity(intent);
+            }
+        });
+
+        NewsFeed =(ImageButton)findViewById(R.id.imageButton);
+        NewsFeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewsFeed.class);
+                startActivity(intent);
+            }
+        });
+        Contact =(ImageButton)findViewById(R.id.imageButton4);
+        Contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Contact.class);
+                startActivity(intent);
+            }
+        });
+        CrtMaterial =(ImageButton)findViewById(R.id.imageButton3);
+        CrtMaterial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CrtMaterial.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
